a4a1c34fd92c15d11264633367c9437113970814
catchwhale <mrjoe8888@gmail.com>
2016-02-13 19:34:24 +0800

    i

--- add_bridge.py
+++ add_bridge.py
@@ -5,7 +5,7 @@ import sys
 
 input = sys.argv[1]
 input = input.strip()
-os.system('ip link add name ' + input ' type dummy')
+os.system('ip link add name ' + input + ' type dummy')
 os.system('ip link set dev ' + input + ' up')
 os.system('ovs-vsctl add-port br-int ' + input)
 
